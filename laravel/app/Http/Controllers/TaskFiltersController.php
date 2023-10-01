e4142d58e7099ef4dbd8a4eeac43f01f49411f00
JCMartinez <jcmtz.091@gmail.com>
2023-09-30 22:59:05 -0600

    injection

--- laravel/app/Http/Controllers/TaskFiltersController.php
+++ laravel/app/Http/Controllers/TaskFiltersController.php
@@ -13,6 +13,10 @@ class TaskFiltersController extends Controller
      */
     public function findTaskByName(Request $request)
     {
+        // Validate the request...
+        $validated = $request->validate([
+            'name' => 'numeric',
+        ]);
         $tasks = DB::table('tasks')
             ->join('republic_states', 'tasks.state_id', '=', 'republic_states.state_id')
             ->select('tasks.*', 'republic_states.state_name')
@@ -27,7 +31,11 @@ class TaskFiltersController extends Controller
      */
     public function findTaskByState(Request $request)
     {
-
+        // Validate the request...
+        $validated = $request->validate([
+            'state_id' => 'numeric',
+        ]);
+        
         $tasks = DB::table('tasks')
             ->join('republic_states', 'tasks.state_id', '=', 'republic_states.state_id')
             ->select('tasks.*', 'republic_states.state_name')
